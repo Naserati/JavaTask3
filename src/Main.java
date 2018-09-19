9ddde1c0628938cd908ce2d41ff5c67b71d58b5e
ANazarov <alexander.nazarov@aplana.com>
2018-09-19 17:56:06 +0300

    Added method "readTextFile"

--- src/Main.java
+++ src/Main.java
@@ -5,47 +5,52 @@ import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        readFile("files/words.txt");
+        readFileAndCountWords("files/words.txt");
     }
 
-    public static void readFile(String filepath){
+    public static void readFileAndCountWords(String filepath) {
+
+        Map<String, Integer> wordsList = new TreeMap<>();
+        String text = readTextFile(filepath);
+
+
+        String[] words = text.split(" ");
+        for (String word : words) {
+            if (!wordsList.containsKey(word)) wordsList.put(word, 1);
+            else wordsList.put(word, wordsList.get(word) + 1);
+        }
+        System.out.println("|Слово|Количество повторений|");
+        String key = null;
+        int value = 0;
+        int maxCount = 0;
+        for (Map.Entry<String, Integer> entry : wordsList.entrySet()) {
+            key = entry.getKey();
+            value = entry.getValue();
+            if (value > maxCount) maxCount = value;
+            System.out.println(String.format("|%s|%d|", key, value));
+        }
+
+        for (Map.Entry<String, Integer> entry : wordsList.entrySet()) {
+            key = entry.getKey();
+            value = entry.getValue();
+            if (value == maxCount)
+                System.out.println(String.format("\nСлово '%s' встречается в файле %d раз(а)", key, value));
+        }
+    }
+
+    public static String readTextFile(String filepath) {
         File file = new File(filepath);
         String text = null;
-        Map<String, Integer> wordsList = new TreeMap<>();
-
         try (FileReader reader = new FileReader(file)) {
 
             char[] buf = new char[256];
             int c;
-            while((c = reader.read(buf))>0){
-                if(c < 256){
+            while ((c = reader.read(buf)) > 0) {
+                if (c < 256) {
                     buf = Arrays.copyOf(buf, c);
                 }
             }
-    
             text = String.valueOf(buf);
-            String[] words = text.split(" ");
-            for (String word : words) {
-                if(!wordsList.containsKey(word)) wordsList.put(word, 1);
-                else wordsList.put(word, wordsList.get(word)+1);
-            }
-            System.out.println("|Слово|Количество повторений|");
-            String key = null;
-            int value = 0;
-            int maxCount = 0;
-            for (Map.Entry<String, Integer> entry : wordsList.entrySet()) {
-               key = entry.getKey();
-               value = entry.getValue();
-               if (value > maxCount) maxCount = value;
-                System.out.println(String.format("|%s|%d|", key, value));
-            }
-
-            for (Map.Entry<String, Integer> entry : wordsList.entrySet()) {
-                key = entry.getKey();
-                value = entry.getValue();
-                if (value == maxCount)
-                System.out.println(String.format("\nСлово '%s' встречается в файле %d раз(а)", key, value));
-            }
 
         } catch (FileNotFoundException e) {
             System.err.println("Файл не найден");
@@ -53,5 +58,6 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return text;
     }
 }
